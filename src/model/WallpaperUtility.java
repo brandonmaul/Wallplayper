0a4b40c8471caf7b3131b0a39993547722d57813
Jasmeet Kaur Chawla <jasmeetk@buffalo.edu>
2018-10-20 23:03:39 -0400

    -Wallpaper changes successfully in windows -Have to make sure you add the jna and jna-platform module -runs a command which downloads the picture in a folder, code then finds the latest downloaded picture in the folder and changes picture through Java Native Access

--- src/model/WallpaperUtility.java
+++ src/model/WallpaperUtility.java
@@ -1,6 +1,11 @@
 package model;
 
+
 import java.io.File;
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+import com.sun.jna.win32.W32APIOptions;
+
 
 
 public class WallpaperUtility {
@@ -11,14 +16,41 @@ public class WallpaperUtility {
         _os = System.getProperty("os.name");
     }
 
+    public static String getLatestFilefromDir(String dirPath){
+        File dir = new File("C:\\Users\\Jasmeet Kaur Chawla\\AppData\\Roaming\\Wallplayper");
+        File[] files = dir.listFiles();
+        if (files == null || files.length == 0) {
+            return null;
+        }
+
+        File lastModifiedFile = files[0];
+        for (int i = 1; i < files.length; i++) {
+            if (lastModifiedFile.lastModified() < files[i].lastModified()) {
+                lastModifiedFile = files[i];
+            }
+        }
+        String name = lastModifiedFile.getName();
+        return name;
+    }
+    public static interface User32 extends Library {
+        User32 INSTANCE = (User32) Native.loadLibrary("user32",User32.class,W32APIOptions.DEFAULT_OPTIONS);
+        boolean SystemParametersInfo (int one, int two, String s ,int three);
+    }
+
     public void setWallpaper(File file) {
         if (_os.startsWith("Windows")) { //WINDOWS
             String command1 = "powershell.exe Set-ItemProperty -path 'HKCU:\\Control Panel\\Desktop\\' -name wallpaper -value \"" + file.getAbsolutePath() + "\"";
-            String command2 = "powershell.exe rundll32.exe user32.dll, UpdatePerUserSystemParameters, 1, True";
+            //Find the path to where the images are getting downloaded
             try{
                 Runtime.getRuntime().exec(command1);
-                Runtime.getRuntime().exec(command2);
-            }catch (Exception e){
+                String path = "C:\\Users\\Jasmeet Kaur Chawla\\AppData\\Roaming\\Wallplayper"; //Change the path here
+                String wallpaper= "C:\\Users\\Jasmeet Kaur Chawla\\AppData\\Roaming\\Wallplayper\\"+ getLatestFilefromDir(path); //add the path back here
+
+                User32.INSTANCE.SystemParametersInfo(0x0014, 0, wallpaper , 1);
+            }
+
+
+            catch (Exception e){
             }
 
         } else if (_os.startsWith("Mac")) { //MAC
